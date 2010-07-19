53f42d8e16af98cdd85417d2232bab54938620b2
Ryan Angilly <ryan@angilly.com>
2010-07-19 12:41:00 -0400

    making it easier to swap out backends

--- lib/less_simple.rb
+++ lib/less_simple.rb
@@ -13,3 +13,38 @@ module I18n
     end
   end
 end
+
+class LessSimple
+  @@klass = I18n::Backend::Simple
+  
+  def self.klass= val
+    @@klass = val
+  end
+
+  def self.klass
+    @@klass
+  end
+
+  def self.factory_backend
+    subclass.new
+  end
+
+  def self.subclass
+    subclass = eval("class LessSimple::#{klass.to_s.gsub('::', '')} < #{klass}; self; end")
+
+    subclass.class_eval do
+      attr_accessor :interpolation_defaults
+      include InstanceMethods
+    end
+  
+    subclass
+  end
+
+  module InstanceMethods
+    def translate(locale, key, options = {})
+      options = (interpolation_defaults || {}).merge options
+      super locale, key, options
+    end
+  end
+
+end
--- test/unit/test_less_simple.rb
+++ test/unit/test_less_simple.rb
@@ -3,34 +3,24 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class TestLessSimple < Test::Unit::TestCase
 
   should "have LessSimple" do
-    assert I18n::Backend::LessSimple
+    assert LessSimple
   end
 
-  context "with a less_simple backend" do
-    setup do
-      @backend = I18n::Backend::LessSimple.new
-      @backend.load_translations(File.join(File.dirname(__FILE__), 'en.yml'))
+  context "On top of the I18n Simple backend" do
+
+    should "default to Simple" do
+      assert_equal I18n::Backend::Simple, LessSimple.klass
     end
 
-    should "have interpolation_defaults" do
-      assert @backend.respond_to?(:interpolation_defaults=)
-      assert @backend.respond_to?(:interpolation_defaults)
-    end
-
-    should "still work" do
-      assert_equal "Oh hi there", @backend.translate('en', 'welcome')
-      assert_equal "MPB Party Planner", @backend.translate('en', 'site', :site_name => 'MPB')
-    end
-
-    should "still throw exceptions on missing values" do
-      assert_raises I18n::MissingInterpolationArgument do
-        puts @backend.translate('en', 'site')
-      end
-    end
-
-    context "with defaults" do
+    context "with a simple backend" do
       setup do
-        @backend.interpolation_defaults = {:site_name => 'skin & bones'}
+        @backend = LessSimple.factory_backend
+        @backend.load_translations(File.join(File.dirname(__FILE__), 'en.yml'))
+      end
+
+      should "have interpolation_defaults" do
+        assert @backend.respond_to?(:interpolation_defaults=)
+        assert @backend.respond_to?(:interpolation_defaults)
       end
 
       should "still work" do
@@ -38,8 +28,25 @@ class TestLessSimple < Test::Unit::TestCase
         assert_equal "MPB Party Planner", @backend.translate('en', 'site', :site_name => 'MPB')
       end
 
-      should "use defaults" do
-        assert_equal 'skin & bones Party Planner', @backend.translate('en', 'site')
+      should "still throw exceptions on missing values" do
+        assert_raises I18n::MissingInterpolationArgument do
+          puts @backend.translate('en', 'site')
+        end
+      end
+
+      context "with defaults" do
+        setup do
+          @backend.interpolation_defaults = {:site_name => 'skin & bones'}
+        end
+
+        should "still work" do
+          assert_equal "Oh hi there", @backend.translate('en', 'welcome')
+          assert_equal "MPB Party Planner", @backend.translate('en', 'site', :site_name => 'MPB')
+        end
+
+        should "use defaults" do
+          assert_equal 'skin & bones Party Planner', @backend.translate('en', 'site')
+        end
       end
     end
   end
